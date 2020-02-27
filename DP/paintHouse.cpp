e058db81d588b339332636db806a894a94738a85
theDataGeek <msarkar@bayshoreintelligence.com>
2020-02-27 12:44:47 +0530

    Changed the char into int for better memoization

--- DP/paintHouse.cpp
+++ DP/paintHouse.cpp
@@ -1,9 +1,9 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-
+// Brute force recursive approach
 namespace Brute {
-    int minCostToPaint(int cost[][3], char lastColor, int curHouse, int n) {
+    int minCostToPaint(int cost[][3], int lastColor, int curHouse, int n) {
         /*
         R G B
         R B G
@@ -13,6 +13,11 @@ namespace Brute {
 
         B R G
         B G R
+
+        // Red - 0
+        // Blue - 1
+        // Green - 2
+        // Other - -1
         */
 
         if (n == 0) {
@@ -24,28 +29,28 @@ namespace Brute {
         int c3 = INT_MAX;
 
         // If the last house was not red
-        if (lastColor != 'r') {
+        if (lastColor != 0) {
             c1 = cost[curHouse][0]; // Choose the red color
-            c1 += minCostToPaint(cost, 'r', curHouse+1, n-1);
+            c1 += minCostToPaint(cost, 0, curHouse+1, n-1);
         }
 
         // If the last house was not blue
-        if (lastColor != 'b') {
+        if (lastColor != 1) {
             c2 = cost[curHouse][1]; // Choose the blue color
-            c2 += minCostToPaint(cost, 'b', curHouse+1, n-1);
+            c2 += minCostToPaint(cost, 1, curHouse+1, n-1);
         }
 
         // If the last house was not green
-        if (lastColor != 'g') {
+        if (lastColor != 2) {
             c3 = cost[curHouse][2]; // Choose the green color
-            c3 += minCostToPaint(cost, 'g', curHouse+1, n-1);
+            c3 += minCostToPaint(cost, 2, curHouse+1, n-1);
         }
 
         return min(c1, min(c2, c3));
     }
 
     int minCostToPaint(int cost[][3], int n) {
-        return minCostToPaint(cost, ' ', 0, n);    
+        return minCostToPaint(cost, -1, 0, n);    
     }
 }
 
