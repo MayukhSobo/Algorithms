cad5028b0e6677560e16ef0589e9bcf55ad19bca
MayukhSobo <mayukh2012@hotmail.com>
2021-12-18 12:22:23 +0530

    backtracking premutations

---
+++ src/backtracking/premuteNumbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> result;
+vector<int> current;
+unordered_map<int, bool> um;
+void _premute(vector<int> &nums, int index) {
+    int n = nums.size();
+    if (index == n) {
+        result.push_back(current);
+        return;
+    }
+
+    for(int i=0; i<n; i++) {
+        if (um[nums[i]] == false) {
+            current.push_back(nums[i]);
+            um[nums[i]] = true; 
+            _premute(nums, index+1);
+            current.pop_back();
+            um[nums[i]] = false;
+        }
+    }
+}
+vector<vector<int>> permute(vector<int>& nums) {
+    _premute(nums, 0);
+    return result;
+}
